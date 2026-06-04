90d21fd77135842696910348dab15d12a580df89
SwayamGupta12345 <Swayamsam2005@gmail.com>
2026-06-04 11:32:04 +0530

    icon improvements

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
   description:
     "Join thousands of people who are revolutionizing their Working experience with collaborative AI assistance with friends.",
   description: "A safe AI demo built for educational purposes. No data stored.",
-   icons: {
+  icons: {
     icon: "/favicon.ico", // optional but explicit
   },
 };
@@ -22,6 +22,23 @@ export default function RootLayout({ children }) {
       <meta
         name="google-site-verification"
         content="Tw09hzfEP_noVIYeF008VqHNPbL-MdVLkUevFXKOpxs"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
       />
       <body className={inter.className} suppressHydrationWarning>
         {children}
